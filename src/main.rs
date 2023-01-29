6192364b6624b12f867b7bc6d6b314bd1430af7d
cornmonger <cornmonger.pub@cornmonger.fish>
2023-01-28 23:17:28 -0800

    cleanup main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use elserpg::model::{zone::{Zone, ZoneTrait}, area::{Area, AreaTrait}, template::{Humanoid, EntityTrait, Entity, HumanoidPart}, DescriptionTrait, entity::EntityComponentTrait};
-
-
+use elserpg::model::{
+    zone::{Zone, ZoneTrait},
+    area::{Area, AreaTrait},
+    template::{Humanoid, EntityTrait, Entity, HumanoidPart},
+    DescriptionTrait,
+    entity::EntityComponentTrait};
 
 fn main() {
     let mut zone = Zone::new(1);
     let lobby = Area::new(zone.generate_id());
     let player = Humanoid::new_player(&mut zone);
 
-    println!("Welcome to {}, {}.", zone.description().unwrap().name(), player.character().entity().description().unwrap().name());
+    println!("Welcome to {}, {}.",
+        zone.description().unwrap().name(),
+        player.character().entity().description().unwrap().name());
+
     println!("You arrive in {}.", lobby.description().unwrap().name());
 
     test_backpack(player.character().entity());
@@ -27,6 +33,5 @@ fn test_backpack(entity: &Entity) {
             .description().unwrap().name(),
         entity
             .component(HumanoidPart::Back as isize).unwrap().entity().unwrap()
-            .description().unwrap().name(),
-    );
+            .description().unwrap().name() );
 }
